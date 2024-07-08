bf00b978a5d645579934aa7a4a58dca2229399a0
Pranav Vichare <pranavvichare361@gmail.com>
2024-07-07 20:00:16 -0500

    Handle get functions

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const express = require("express");
-
+const { connectToMongoDB } = require("./connect");
+const urlRoute = require("./routes/url");
 const app = express();
 const port = 8001;
 
-app.listen(PORT, () => console.log(`Server started at port:${port}`));
+connectToMongoDB("mongodb://localhost:27017/short-url").then(() =>
+  console.log("mongodb connected")
+);
+app.use("/url", urlRoute);
+app.listen(port, () => console.log(`Server started at port:${port}`));
