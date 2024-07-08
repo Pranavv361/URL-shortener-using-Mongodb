c06e9b3358509f1c888c039bd48a01e13aad8d83
Pranav Vichare <pranavvichare361@gmail.com>
2024-07-07 21:06:26 -0500

    Update index file

--- index.js
+++ index.js
@@ -7,5 +7,6 @@ const port = 8001;
 connectToMongoDB("mongodb://localhost:27017/short-url").then(() =>
   console.log("mongodb connected")
 );
+app.use(express.json());
 app.use("/url", urlRoute);
 app.listen(port, () => console.log(`Server started at port:${port}`));
