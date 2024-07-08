55c47c18c09cc4396a535b709fe2026b6e09abf0
Pranavv361 <pranavvichare361@gmail.com>
2024-07-08 15:25:42 -0500

    Complete the working logic

--- index.js
+++ index.js
@@ -1,12 +1,31 @@
 const express = require("express");
+const path = require("path");
 const { connectToMongoDB } = require("./connect");
 const urlRoute = require("./routes/url");
+const URL = require("./models/url");
 const app = express();
 const port = 8001;
 
 connectToMongoDB("mongodb://localhost:27017/short-url").then(() =>
   console.log("mongodb connected")
 );
+
+app.set("view engine", "ejs");
+app.set("views", path);
 app.use(express.json());
 app.use("/url", urlRoute);
+app.get("/:shortId", async (req, res) => {
+  const shortId = req.params.shortId;
+  const entry = await URL.findOneAndUpdate(
+    {
+      shortId,
+    },
+    {
+      $push: {
+        visitHistory: { timestamp: Date.now() },
+      },
+    }
+  );
+  res.redirect(entry.redirectURL);
+});
 app.listen(port, () => console.log(`Server started at port:${port}`));
