0931d460c797639841f785c70af41b149c937d96
Pranavv361 <pranavvichare361@gmail.com>
2024-07-09 00:08:41 -0500

    Add Authentication code

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ const express = require("express");
 const path = require("path");
 const { connectToMongoDB } = require("./connect");
 const urlRoute = require("./routes/url");
+const staticRoute = require("./routes/staticRouter");
 const URL = require("./models/url");
+
 const app = express();
 const port = 8001;
 
@@ -14,15 +16,12 @@ app.set("view engine", "ejs");
 app.set("views", path.resolve("./views"));
 
 app.use(express.json());
-
-app.get("/test", async (req, res) => {
-  const allUrls = await URL.find({});
-  return res.render("home", {
-    urls: allUrls,
-  });
-});
+app.use(express.urlencoded({ extended: false }));
 
 app.use("/url", urlRoute);
+
+app.use("/", staticRoute);
+
 app.get("/url/:shortId", async (req, res) => {
   const shortId = req.params.shortId;
   const entry = await URL.findOneAndUpdate(
